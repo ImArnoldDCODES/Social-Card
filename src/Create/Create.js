350d0799c5d0d5e9ba01f610fdd19c791b84e453
ImArnoldDCODES <danielsarnold010@gmail.com>
2022-11-11 22:51:08 -0800

    Im back

--- src/Create/Create.js
+++ src/Create/Create.js
@@ -1,39 +1,46 @@
-import React, {useState} from 'react'
-import {useForm} from 'react-hook-form'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 export default function Create() {
 
     const [firstName, setFirstName] = useState()
     const [lastName, setLastName] = useState()
+    const [ig, setIg] = useState()
 
-    const {register, handleSubmit} = useForm()
+    const { register, handleSubmit } = useForm()
 
     const onSubmit = (f) => {
         alert(JSON.stringify(f))
     }
 
     console.log(lastName)
+    
 
-  return (
-    <div>
-        <h3>Create joor</h3>
+    return (
+        <div>
+            <h3>Create joor</h3>
 
-        <form onSubmit={handleSubmit}>
-            <div>
-            <label>FirstName</label>
-            <input placeholder='FirstName' onChange={text => setFirstName(text.target.value)}/>
-            </div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>FirstName</label>
+                    <input placeholder='FirstName' onChange={text => setFirstName(text.target.value)} />
+                </div>
 
-            <div>
-            <label>LastName</label>
-            <input placeholder='LastName' onChange={(text) => {setLastName(text.target.value)}}/>
-            </div>
-{/* 
+                <div>
+                    <label>LastName</label>
+                    <input placeholder='LastName' onChange={(text) => { setLastName(text.target.value) }} />
+                </div>
+
+                <div>
+                    <label>Instagram</label>
+                    <input placeholder='Instagram Link' onChange={(text) => setIg(text.target.value)}></input>
+                </div>
+                {/* 
             <div>
             <label>LastName</label>
             <input placeholder='LastName'/>
             </div> */}
-        </form>
-    </div>
-  )
+            </form>
+        </div>
+    )
 }
