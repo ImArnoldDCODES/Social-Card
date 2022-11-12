633cffe9a0120d8fd09cbcba8f1d20851f61a955
ImArnoldDCODES <danielsarnold010@gmail.com>
2022-11-12 00:05:59 -0800

    created routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css'
 import Land from './Land/Land';
 import Create from './Create/Create';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App"> 
-      <Create />
-      {/* <Land /> */}
-    </div>
+    <Router>
+    <Routes>
+    <Route path='/' element={<Land />}/>
+    <Route path='/create' element={<Create />}/>
+    </Routes>
+    </Router>
   );
 }
 
--- src/Land/Land.js
+++ src/Land/Land.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './styles.css'
+import { NavLink } from 'react-router-dom'
 
 export default function Land() {
   return (
     <div className='content'>
         <div className='text'>
             <h1>Hello, make social cards for your business within 2 mins</h1>
-
-            <button>Get Started</button>
+            <NavLink to='/create'>
+            Get Started
+            </NavLink>
         </div>
         <div className='icon'>
             <h1>/</h1>
