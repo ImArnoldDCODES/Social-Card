4bdcd2424a99779b6ecd61426dc2ffe745666a90
ImArnoldDCODES <danielsarnold010@gmail.com>
2022-11-28 21:20:56 -0800

    working on card

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import './styles.css'
 
 export default function Card() {
 
+  const [file, setFile] = useState();
+  function handleChange(e) {
+    console.log(e.target.files);
+    setFile(URL.createObjectURL(e.target.files[0]));
+  }
+
   const names = useSelector((state) => state.names)
   console.log(names)
   return (
-    <div>
-        <h1>Hola</h1>
+    <div className='container'>
+      <div className='card'>
         {names.map((details, index) => (
           <div key={index}>
-            {/* <h3>{details.firstname}</h3>
-            <h2>{details.lastname}</h2> */}
-           <h1>{details[0].firstname}</h1>
+            {/* <h3>{details.firstname}</h3>*/}
+            {/* <h2>{details[0].lastname}</h2>  */}
+            <h1>{details[0].firstname}</h1>
+
           </div>
         ))}
-    </div>  
+        {/* <h2>Add Image:</h2>
+        <input type="file" onChange={handleChange} />
+        <img src={file} alt='' /> */}
+
+      </div>
+    </div>
   )
 }
